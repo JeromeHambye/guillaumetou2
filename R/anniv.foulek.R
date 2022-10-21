b5cd02e5a03568177c3900847006ec5d29055006
JeromeHambye <hambye.jerome@gmail.com>
2022-10-21 22:22:06 +0200

    Fonction v1
    Création fonction v1

---
+++ R/anniv.foulek.R
@@ -0,0 +1,9 @@
+#' Pour afficher un message d'anniversaire dans R
+#'
+#' @return Crache pas du picon au plafond sale foulek
+#' @export
+#'
+#' @examples anniv.foulek()
+anniv.foulek <- function(){
+  print("Bon anniversaire sale foulek")
+}
